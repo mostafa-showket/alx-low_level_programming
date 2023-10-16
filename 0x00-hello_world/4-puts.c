66cdf879f73da1ce9019bd2206a566022613645b
mostafa_showket <mostafashowket2@gmail.com>
2023-10-16 10:31:31 -0700

    finished main tasks
    added files 2-assempler 3-name 4-puts.c 5-printf.c 6-size.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ *
+ * Description: prints string using puts()
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ *
+ * Description: prints string using printf()
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
