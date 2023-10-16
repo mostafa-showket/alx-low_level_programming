d346fd91617f7696481f9954839aefa1bee0ac7f
mostafa_showket <mostafashowket2@gmail.com>
2023-10-16 11:08:51 -0700

    added advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * Description: prints quote
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	putchar("and that piece of art is useful \" - Dora Korper, 2015-10-19\n");
+	return (1);
+}
