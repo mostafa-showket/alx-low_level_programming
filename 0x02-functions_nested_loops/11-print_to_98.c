f1cc7b63f3e9ac2172c7cf57a393f740e955e870
mostafa_showket <mostafashowket2@gmail.com>
2023-10-18 15:09:54 -0700

    fixed files output

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,28 +8,17 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n < 98)
 	{
-		while (n <= 98)
-		{
-			if (n == 98)
-				_putchar(n);
-			else
-				printf("%d,", n);
-			n++;
-		}
-	} else if (n == 98)
-	{
-		printf("%d", n);
-	} else
-	{
-		while (n >= 98)
-		{
-			if (n == 98)
-				_putchar(n)
-			else
-				printf("%d, ", n);
-			n--;
-		}
+		printf("%d,", n);
+		n++;
 	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	if (n == 98)
+		printf("%d",n);
+	printf("\n");
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,15 +10,27 @@ void times_table(void)
 {
 	int i, j, result;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			_putchar('0' + result);
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			if (j == 0)
+			{
+				_putchar('0' + result);
+			} else if (result <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			} else if (result > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
 		}
 		_putchar('\n');
 	}
