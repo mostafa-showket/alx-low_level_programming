a1db4fda7515a49e1758b3c694a4b5cbd73875c9
mostafa_showket <mostafashowket2@gmail.com>
2023-10-25 13:07:17 -0700

    added 0x03-debugging

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,9 @@
+0x03. C - Debugging
+
+0-main.c: this file test the function `positive_or_negative()` gives the correct output when given a case of `0`
+
+1-main.c: avoid the inifinte loop
+
+2-largest_number.c: prints the largest of three numbers
+
+3-print-remaining_days.c: print the number of days till the end of the year
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * main - prints random number's type
+ *
+ * Description: prints random number and
+ * if that random number is positive or negative
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int i);
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
+
+/**
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
+
+void print_remaining_days(int month, int day, int year);
+
+#endif
