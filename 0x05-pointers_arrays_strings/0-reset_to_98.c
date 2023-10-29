44a1f3ce5e6fc3033544934b6c12215a7566cafe
mostafa_showket <mostafashowket2@gmail.com>
2023-10-29 15:44:17 -0700

    added file 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the parameter it points to
+ * @n: pointer to the parameter to be updated
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define main_h
+/**
+ * reset_to_98 - updates the value of the parameter it points to
+ * @n: pointer to the parameter to be updated
+ */
+
+void reset_to_98(int *n);
+
+#endif
