d5cef8b2ef9fe36fa98bf9e3f110fe045f906ee9
Mostafa Showket <mostafashowket2@gmail.com>
2023-11-05 00:20:44 +0200

    added file 0-strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to be concatenated to
+ * @src: string to be added to dest
+ *
+ * Return: pointer to resulting string dest
+ */
+
+char *_strcat(char *dest, char *src);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
