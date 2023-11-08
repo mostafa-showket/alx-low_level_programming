666487856f198e757897a54600204dcc78f12519
Mostafa Showket <mostafashowket2@gmail.com>
2023-11-08 11:47:26 +0200

    changed code to get right output

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,48 +1,76 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers
- * @n1: number to be added
- * @n2: number to be added
- * @r: the buffer to store the result
- * @size_r: the buffer size
- *
- * @Return: pointer to the result
- */
+* rev_string - reverse array
+* @n: integer params
+* Return: 0
+*/
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+		i++;
+	i--;
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+* infinite_add - add 2 numbers together
+* @n1: text representation of 1st number to add
+* @n2: text representation of 2nd number to add
+* @r: pointer to buffer
+* @size_r: buffer size
+* Return: pointer to calling function
+*/
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, a, b, c, n, aux, dec = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (n1[i] != '\0')
+	while (*(n1 + i) != '\0')
 		i++;
-	while (n2[j] != '\0')
+	while (*(n2 + j) != '\0')
 		j++;
-	for (n = 0; n < j || n < i; n++)
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		a = (i - n) > 0 ? (n1[i - n - 1] - '0') : 0;
-		b = (j - n) > 0 ? (n2[j - n - 1] - '0') : 0;
-		c = a + b + dec;
-		r[n] = (c % 10) + '0';
-		dec = c > 9 ? 1 : 0;
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i)-'0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j)-'0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	if (dec == 1)
-	{
-	       	r[n] = '1';
-		r[n + 1] = '\0'; }
-	else
-	{
-		r[n] = '\0';
-		n--;
-	}
-	for (i = 0; i < n + 1; i++)
-	{
-		for (j = 0; j < (n - i); j++)
-		{
-			aux = r[j + 1];
-			r[j + 1] = r[j];
-			r[j] = aux;
-		}
-	}
-	return (n < size_r - 1 ? r : 0);
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
+
