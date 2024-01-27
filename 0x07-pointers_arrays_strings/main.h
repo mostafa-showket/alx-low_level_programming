26605b397aafc5c8427717bcfcaaa0bf286c7c03
Mostafa Showket <mostafashowket2@gmail.com>
2024-01-27 23:07:26 +0200

    added file 1-memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,4 +12,15 @@
 
 char *_memset(char *s, char b, unsigned int n);
 
+/**
+ * _memcpy - copies memory data from area to another
+ * @dest: memory area to be copied to
+ * @src: memory area copied data from
+ * @n: number of bytes to be copied
+ *
+ * Return: a pointer to memory area dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 #endif
